98948b8eb21c9e3d61162fe63ab91646f9212dc7
Samuel Morales Jr <smoralesjr@gmail.com>
2018-11-01 10:36:44 -0400

    test function

--- js/script.js
+++ js/script.js
@@ -11,7 +11,26 @@ const studentItem = document.querySelectorAll(".student-item");
 // Create a function to hide all of the items in the list except for the ten you want to show
 // Tip: Keep in mind that with a list of 54 studetns, the last page will only display four
 
+//Testing here hiding everything except for the first 10. Delete later
 
+const displayStudents = (studentStart, studentEnd) => {
+
+  for (let count = 0; count < studentItem.length; count++) {
+    studentItem[count].style.display = "none";
+
+  }
+
+  for (let count = studentStart; count < studentEnd; count++){
+    studentItem[count].style.display = "block";
+
+  }
+
+};
+
+// for (count = 10; count < 54; count++) {
+//   studentItem[count].style.display = "none";
+//
+// }
 
 
 // Create and append the pagination links - Creating a function that can do this is a good approach
@@ -21,3 +40,5 @@ const studentItem = document.querySelectorAll(".student-item");
 
 // Add functionality to the pagination buttons so that they show and hide the correct items
 // Tip: If you created a function above to show/hide list items, it could be helpful here
+
+displayStudents(0, 10);
